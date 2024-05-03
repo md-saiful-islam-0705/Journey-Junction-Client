5d470de483c0d91f0582fd74d42eae66eedf114f
Md Saiful Islam <mdsaifulislam@Mds-MacBook-Air.local>
2024-05-04 02:35:40 +0300

    added contact us button and React-simple-typewriter

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,8 +1,11 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
 const Banner = () => {
   return (
     <div className="carousel w-full lg:rounded-2xl md:rounded-xl">
       <div id="slide1" className="carousel-item relative w-full">
-        <img src="https://i.ibb.co/qB6dBdk/banner-3.jpg" className="w-full" />
+        <img src="https://i.ibb.co/qB6dBdk/banner-3.jpg" className="w-full" alt="Slide 1" />
         <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
           <a href="#slide4" className="btn btn-circle">
             ❮
@@ -11,12 +14,16 @@ const Banner = () => {
             ❯
           </a>
         </div>
+        <div className="absolute bottom-5 left-0 right-0 text-center">
+          <Link to="/about">
+            <button className="  bg-gradient-to-r from-violet-500 to-pink-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
+              Contact Us
+            </button>
+          </Link>
+        </div>
       </div>
       <div id="slide2" className="carousel-item relative w-full">
-        <img
-          src="https://i.ibb.co/QrPzFj8/banner-2.jpg"
-          className="w-full"
-        />
+        <img src="https://i.ibb.co/QrPzFj8/banner-2.jpg" className="w-full" alt="Slide 2" />
         <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
           <a href="#slide1" className="btn btn-circle">
             ❮
@@ -27,7 +34,7 @@ const Banner = () => {
         </div>
       </div>
       <div id="slide3" className="carousel-item relative w-full">
-        <img src="https://i.ibb.co/swLb5jR/banner-4.jpg" className="w-full" />
+        <img src="https://i.ibb.co/swLb5jR/banner-4.jpg" className="w-full" alt="Slide 3" />
         <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
           <a href="#slide2" className="btn btn-circle">
             ❮
@@ -36,6 +43,7 @@ const Banner = () => {
             ❯
           </a>
         </div>
+        
       </div>
     </div>
   );
--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+import { Typewriter } from 'react-simple-typewriter';
 import Footer from "../shared/Footer";
 import Navbar from "../shared/Navbar";
 import { Helmet } from "react-helmet";
@@ -16,40 +18,108 @@ const About = () => {
               <h2 className="text-3xl font-bold mb-4">
                 Welcome to{" "}
                 <span className="text-gradient bg-gradient-to-r from-violet-600 to-pink-500 text-transparent bg-clip-text">
-                  Journey Junction
+                  <Typewriter
+                    words={['Journey Junction']}
+                    loop={false}
+                    cursor={false}
+                    typeSpeed={30}
+                    deleteSpeed={10}
+                    delaySpeed={2000}
+                  />
                 </span>
               </h2>
               <div>
                 <p className="text-lg text-gray-700 mb-6">
-                  journey junction At Crestview Properties, we pride ourselves
+                  Journey Junction At Crestview Properties, we pride ourselves
                   on offering a diverse range of properties and unparalleled
                   service to our clients. Whether you are in the market to buy,
                   sell, or rent, we are here to assist you every step of the
                   way.
                 </p>
                 <p className="text-lg text-gray-700 mb-6">
-                  journey junction Our dedicated team of professionals is
+                  Journey Junction Our dedicated team of professionals is
                   committed to helping you find the perfect property to call
                   home. With our easy-to-use website and comprehensive search
                   tools, discovering your dream property is simple and
                   hassle-free.
                 </p>
                 <p className="text-lg text-gray-700 mb-6">
-                  journey junction Explore Crestview Properties to browse our
+                  Journey Junction Explore Crestview Properties to browse our
                   latest listings, access helpful resources, and gain expert
                   insights into the real estate market. Join our growing
                   community of satisfied clients who have experienced the
                   Crestview difference.
                 </p>
                 <p className="text-lg text-gray-700">
-                  journey junction Contact us today to embark on your real
+                  Journey Junction Contact us today to embark on your real
                   estate journey with Crestview Properties!
                 </p>
               </div>
             </div>
-            <h1 className="text-3xl font-bold text-purple-600 text-center">
-              Our location
-            </h1>
+
+            <div className="container mx-auto px-4 py-8">
+              <span className="text-3xl font-bold text-gradient bg-gradient-to-r from-violet-600 to-pink-500 text-transparent bg-clip-text">
+                <Typewriter
+                  words={['Contact Us']}
+                  loop={false}
+                  cursor={false}
+                  typeSpeed={70}
+                  deleteSpeed={50}
+                  delaySpeed={1000}
+                />
+              </span>
+              <p className="text-lg mb-4">Feel free to get in touch with us!</p>
+              <form className="max-w-md border p-4 rounded-md">
+                <div className="mb-4">
+                  <label
+                    htmlFor="name"
+                    className="block text-sm font-semibold mb-2"
+                  >
+                    Name
+                  </label>
+                  <input
+                    type="text"
+                    id="name"
+                    name="name"
+                    className="w-full px-3 py-2 border rounded-md focus:outline-none focus:ring focus:border-blue-500"
+                  />
+                </div>
+                <div className="mb-4">
+                  <label
+                    htmlFor="email"
+                    className="block text-sm font-semibold mb-2"
+                  >
+                    Email
+                  </label>
+                  <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    className="w-full px-3 py-2 border rounded-md focus:outline-none focus:ring focus:border-blue-500"
+                  />
+                </div>
+                <div className="mb-4">
+                  <label
+                    htmlFor="message"
+                    className="block text-sm font-semibold mb-2"
+                  >
+                    Message
+                  </label>
+                  <textarea
+                    id="message"
+                    name="message"
+                    rows="4"
+                    className="w-full px-3 py-2 border rounded-md focus:outline-none focus:ring focus:border-blue-500"
+                  ></textarea>
+                </div>
+                <button
+                  type="submit"
+                  className="bg-purple-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
+                >
+                  Submit
+                </button>
+              </form>
+            </div>
           </div>
         </div>
         <Footer />
